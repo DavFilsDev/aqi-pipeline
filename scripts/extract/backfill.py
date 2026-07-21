9ef7681c0a8a1c39b7eb840b4dd400510c63bc2d
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-07-21 17:49:11 +0300

    feat:(backfill.py): transform french to english

--- scripts/extract/backfill.py
+++ scripts/extract/backfill.py
@@ -6,9 +6,7 @@ from scripts.extract.extract_aqi import (
     extract_aqi
 )
 
- 
 BACKFILL_MONTHS = 3
- 
 
 logging.basicConfig(
     level=logging.INFO,
@@ -19,39 +17,35 @@ logging.basicConfig(
     )
 )
 
- 
 
 def get_start_date(months: int) -> datetime:
     """
-    Retourne la date de début du backfill.
+    Returns the start date for the backfill.
     """
 
     now = datetime.now(timezone.utc)
 
     if months == 0:
-
         return now - timedelta(hours=2)
-
 
     return now - timedelta(
         days=months * 30
     )
 
 
-
 def get_end_date() -> datetime:
     """
-    Retourne la date de fin du backfill.
+    Returns the end date for the backfill.
     """
 
     return datetime.now(timezone.utc)
 
- 
 
 def normalize_hour(date: datetime) -> datetime:
     """
-    Transforme une date en heure pile.
-    Exemple:
+    Rounds a datetime down to the beginning of the hour.
+
+    Example:
     17:20:35 -> 17:00:00
     """
 
@@ -60,12 +54,11 @@ def normalize_hour(date: datetime) -> datetime:
         second=0,
         microsecond=0
     )
- 
+
 
 def run_backfill():
 
     cities = load_cities()
-
 
     start_date = normalize_hour(
         get_start_date(
@@ -73,20 +66,16 @@ def run_backfill():
         )
     )
 
-
     end_date = normalize_hour(
         get_end_date()
     )
 
-
     logging.info(
-        f"Backfill de {start_date} "
-        f"à {end_date}"
+        f"Backfill from {start_date} "
+        f"to {end_date}"
     )
 
-
     current_date = start_date
-
 
     total_hours = int(
         (
@@ -95,42 +84,31 @@ def run_backfill():
         // 3600
     )
 
-
     logging.info(
-        f"Nombre d'heures à traiter : {total_hours}"
+        f"Total hours to process: {total_hours}"
     )
-
-
 
     while current_date <= end_date:
 
-
         logging.info(
-            f"Traitement date : {current_date}"
+            f"Processing date: {current_date}"
         )
 
-
         for city in cities:
-
 
             extract_aqi(
                 city,
                 current_date
             )
 
-
-
         current_date += timedelta(
             hours=1
         )
 
-
-
     logging.info(
-        "Backfill terminé avec succès"
+        "Backfill completed successfully"
     )
 
- 
 
 if __name__ == "__main__":
 
